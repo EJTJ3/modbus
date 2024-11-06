3754b65568c86ef6a2cea2a59b270418dcccccc4
Evert Jan Hakvoort <evertjan03@gmail.com>
2024-11-06 21:47:02 +0100

    fix: panic when transport is nil

--- modbus.go
+++ modbus.go
@@ -70,6 +70,7 @@ const (
 	ErrBadTransactionId          Error = "bad transaction id"
 	ErrUnknownProtocolId         Error = "unknown protocol identifier"
 	ErrUnexpectedParameters      Error = "unexpected parameters"
+	ErrUndefinedTransport        Error = "Transport is undefined"
 )
 
 // mapExceptionCodeToError turns a modbus exception code into a higher level Error object.
